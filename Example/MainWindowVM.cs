69128fb8b300922263d54d1c0220b779c99aeb87
Alexander <zam.kuratora@gmail.com>
2019-05-06 19:02:07 +0300

    New version: 1.0.1.0
    Added: MessageBox

--- Example/MainWindowVM.cs
+++ Example/MainWindowVM.cs
@@ -1,5 +1,7 @@
 ﻿using DevExpress.Mvvm;
 using MaterialDesignXaml.DialogsHelper;
+using MaterialDesignXaml.DialogsHelper.Enums;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -14,11 +16,36 @@ namespace Example
         /// </summary>
         public ICommand OpenDialogCommand => new DelegateCommand(async () =>
         {
-            //...any works
+            //...any work
 
-            string name = await this.Show<string>(new TestDialog(this));
+            string name = await this.ShowAsync<string>(new TestDialog(this), () => System.Console.WriteLine("Hi"), () => System.Console.WriteLine("Bye"));
 
             MessageBox.Show($"Your name: {name}");
+        });
+
+        /// <summary>
+        /// DelegateCommand - DevExpressMVVM.
+        /// </summary>
+        public ICommand Open2SecDialogCommand => new DelegateCommand(async () =>
+        {
+            await this.ShowAsync("This dialog closes within 2 seconds.", async () =>
+            {
+                await Task.Delay(2000);
+
+                this.Close();
+            });
+        });
+
+        /// <summary>
+        /// DelegateCommand - DevExpressMVVM.
+        /// </summary>
+        public ICommand OpenMessageBox => new DelegateCommand(async () =>
+        {
+            //...any work
+
+            var result = await this.ShowMessageBoxAsync("What you'll press?", MaterialMessageBoxButtons.All);
+
+            await this.ShowMessageBoxAsync($"You pressed: {result}", MaterialMessageBoxButtons.Ok);
         });
     }
 }
